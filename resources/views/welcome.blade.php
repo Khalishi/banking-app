e9b2c91a47706d3702af53f4b7bd69c375df12b5
= <=>
2022-12-31 14:08:32 +0200

    set up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Banking-app</title>
 </head>
 <body>
-<div class="flex ...">
-  <div class="flex-1 ...">01</div>
-  <div class="contents">
-    <div class="flex-1 ...">02</div>
-    <div class="flex-1 ...">03</div>
-  </div>
-  <div class="flex-1 ...">04</div>
+<div class="flex bg-green-500">
+  <div class="hidden ...">01</div>
+  <div>02</div>
+  <div>03</div>
 </div>
 </body>
 </html>
