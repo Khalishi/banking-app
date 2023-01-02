34455f7d780858cdded82463b9a619515d1ed487
= <=>
2023-01-02 15:55:46 +0200

    udate the header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Banking-app</title>
 </head>
 <body>
-<div class="flex bg-green-500">
-  <div class="hidden ...">01</div>
-  <div>02</div>
-  <div>03</div>
-</div>
+<header>
+  <div>
+     <nav>
+       <ul class="flex space-x-4 ml-80 pl-96 pt-4">
+          <li>Login</li>
+          <li>Register</li>
+       </ul>
+     </nav>
+     <h1>FBIF</h1>
+    <nav>
+        <ul class="bg-black flex text-white items-center pt-4 pb-4 space-x-8 pl-72 ">
+            <li>Home</li>
+            <li>Pay</li>
+            <li>Cards</li>
+            <li>Prepaid mobile</li>
+            <li>Buy electricity</li>
+            <li>Transfer money</li>
+            <li>Open account</li>
+            <li>Careers</li>
+            <li>Loan</li>
+            <li>Invest</li>
+            <li>About us</li>
+            <li>Contact us</li>
+        </ul>
+      </nav>
+  
+  </div>   
+
+</header>
+    
 </body>
 </html>
