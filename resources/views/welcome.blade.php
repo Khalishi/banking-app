7318cbf8a87006303b59fda4c60ff72a8f292682
= <=>
2023-01-03 15:36:41 +0200

    work on home page or welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,32 +12,118 @@
   <div>
      <nav>
        <ul class="flex space-x-4 ml-80 pl-96 pt-4">
-          <li>Login</li>
-          <li>Register</li>
+          <li>
+            <a class="hover:bg-gray-600" href="#">Login</a>
+        </li>
+          <li>
+            <a class="hover:bg-gray-600" href="#">Register</a>
+        </li>
        </ul>
      </nav>
      
     <nav>
-      <h1>FBIF</h1>
-        <ul class="bg-black flex text-white items-center pt-4 pb-4 space-x-8 pl-72 ">
-            <li>Home</li>
-            <li>Pay</li>
-            <li>Cards</li>
-            <li>Prepaid mobile</li>
-            <li>Buy electricity</li>
-            <li>Transfer money</li>
-            <li>Open account</li>
-            <li>Careers</li>
-            <li>Loan</li>
-            <li>Invest</li>
-            <li>About us</li>
-            <li>Contact us</li>
+        <ul class="bg-black flex text-white items-center pt-4 pb-4 space-x-8 pl-72">
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Home</a>
+            </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Pay</a>
+            </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Cards</a>
+            </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Prepaid mobile</a>
+            </li>
+            <li>
+               <a class="hover:bg-gray-600 rounded-full" href="#">Buy electricity</a>
+            </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Transfer money</a>
+          </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Open account</a>
+          </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Careers</a>
+          </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Loan</a>
+          </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Invest</a>
+          </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">About us</a>
+          </li>
+            <li>
+              <a class="hover:bg-gray-600 rounded-full" href="#">Contact us</a>
+          </li>
         </ul>
+
+        <h1 class="text-xl font-bold text-green-500 bg-gray-500">FBIF</h1>
+
       </nav>
   
-  </div>   
-
+   </div>  
 </header>
+
+<section>
+  
+    <div class="grid grid-cols-3 gap-14 pl-4 pt-20">
+      <div>
+          <img src="/images/first.jpg" alt="first">
+      </div>
     
+      <div>
+        <img src="/images/second.jpg" alt="first">
+      </div>
+
+        <div>
+          <img src="/images/third.jpg" alt="first">
+        </div>
+
+  </div>
+
+  <div class="grid grid-cols-3 gap-14 pl-4 pt-8 text-xl font-bold">
+        <div>
+            Start banking and invest with us
+        </div>
+      
+        <div>
+            Open a savings account with us with a low interest
+        </div>
+
+          <div>
+            Get a black card and start flexing
+         </div>
+</section>
+
+
+<footer class="bg-black border border-black border-opacity-5 text-center py-16 px-10 mt-16">
+        
+            <h5 class="text-xl font-bold text-white">Subscibe to our newsletter</h5>
+
+            <div class="mt-10">
+                <div class="relative inline-block mx-auto lg:bg-gray-200">
+
+                    <form method="POST" action="#" class="lg:flex text-sm">
+                        <div class="lg:py-3 lg:px-5 flex items-center">
+                            <label for="email" class="hidden lg:inline-block">
+                            </label>
+
+                            <input id="email" type="text" placeholder="Your email address"
+                                   class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+                        </div>
+
+                        <button type="submit"
+                                class="transition-colors duration-300 bg-black hover:bg-gray-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8"
+                        >
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </footer>
 </body>
 </html>
