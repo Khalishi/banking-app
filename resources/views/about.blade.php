0b8e7e14ed9fb74b94555d231b3ba6f5021c9767
= <=>
2023-01-10 11:22:53 +0200

    update contact page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,14 +1,14 @@
 <x-layout>
-    <h1 class="font-bold pl-4 pt-4">ABOUT US</h1>
+    <h1 class="font-bold pl-4 pt-4 bg-blue-200 pt-40 text-blue-700">ABOUT US</h1>
       <div class="grid grid-cols-2 gap-14 pl-4 pt-8 text-xl">
-            <div class="bg-gray-200 rounded-xl">
+            <div class="bg-green-300 rounded-xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
                 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
                 publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </div>
-            <div class="bg-gray-200 rounded-xl">
+            <div class="bg-green-300 rounded-xl">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
