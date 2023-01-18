cc8941a2418dd567bbfdd05b6001b95a6e8c5252
= <=>
2023-01-18 23:32:05 +0200

    work on dropdown/component

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,43 +1,21 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white'])
+<div x-data="{ show: false }" @click.away = "show = false">
+    
+    {{-- Trigger --}}
+    <button 
+        @click="show = ! show" 
+        class="py-2 pl-3 pr-9 text-sm font-semibold flex"
+        > 
+            <svg class="w-4 h-4 ml-1" aria-hidden="true" fill="currentColor" 
+                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path 
+                    fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" 
+                        clip-rule="evenodd"
+                    ></path>
+            </svg>
+    </button>
 
-@php
-switch ($align) {
-    case 'left':
-        $alignmentClasses = 'origin-top-left left-0';
-        break;
-    case 'top':
-        $alignmentClasses = 'origin-top';
-        break;
-    case 'right':
-    default:
-        $alignmentClasses = 'origin-top-right right-0';
-        break;
-}
+    {{-- LINKS --}}
 
-switch ($width) {
-    case '48':
-        $width = 'w-48';
-        break;
-}
-@endphp
-
-<div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
-    <div @click="open = ! open">
-        {{ $trigger }}
-    </div>
-
-    <div x-show="open"
-            x-transition:enter="transition ease-out duration-200"
-            x-transition:enter-start="transform opacity-0 scale-95"
-            x-transition:enter-end="transform opacity-100 scale-100"
-            x-transition:leave="transition ease-in duration-75"
-            x-transition:leave-start="transform opacity-100 scale-100"
-            x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
-            style="display: none;"
-            @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
-            {{ $content }}
-        </div>
+    <div x-show="show" class="py-2 absolute text-black bg-blue-200 mt-2 z-50" style="display: none">
+         {{ $slot }}
     </div>
 </div>
