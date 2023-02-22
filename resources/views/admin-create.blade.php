77d7574d9d710726161361ef923af87101d3999a
= <=>
2023-02-22 05:46:11 +0200

    handle contact create validation errors

--- resources/views/admin-create.blade.php
+++ resources/views/admin-create.blade.php
@@ -1,37 +1,76 @@
 <x-layout>
         <!-- Contact form -->
-        <div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12 bg-blue-400">
+        <div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12 bg-blue-200">
             <h3 class="text-lg font-medium text-warm-gray-900">Create Contact-Info</h3>
                   <form action="/admin/contact-info/create" method="POST" class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
                          @csrf
                       <div>
                         <label for="title" class="block text-lg font-medium text-warm-gray-900">Title</label>
                       <div class="mt-1">
-                      <input type="text" name="title" id="title" autocomplete="given-name" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-600 focus:border-blue-600 border-warm-gray-300 rounded-md">
+                      <input type="text" 
+                       name="title" 
+                       id="title" 
+                       value="{{ old('title') }}"
+                       autocomplete="given-name" 
+                       class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-600 focus:border-blue-600 border-warm-gray-300 rounded-md"
+                       >
+
+                      @error('title')
+                          <p class="text-red-500 text-lg">{{ $message }}</p>
+                      @enderror
                   </div>
+                 
                 </div>
 
                 <div class="sm:col-span-2">
                     <div class="flex justify-between">
                       <label for="description" class="block text-lg font-medium text-warm-gray-900">Description</label>
-                      <span id="description-max" class="text-sm text-warm-gray-500">Max. 250 characters</span>
+                      <span id="description-max" class="text-sm text-warm-gray-500">Max. 255 characters</span>
                     </div>
                     <div class="mt-1">
-                      <textarea id="description" name="description" rows="4" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-600 focus:border-blue-600 border border-warm-gray-300 rounded-md" aria-describedby="message-max"></textarea>
+                      <textarea id="description" 
+                      name="description" 
+                      rows="2" 
+                      class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-600 focus:border-blue-600 border border-warm-gray-300 rounded-md" 
+                      aria-describedby="message-max">
+                    </textarea>
+                      @error('title')
+                          <p class="text-red-500 text-lg">{{ $message }}</p>
+                      @enderror
                     </div>
                   </div>
 
              <div>
                 <label for="contact number" class="block text-lg font-medium text-warm-gray-900">Contact number</label>
                     <div class="mt-1">
-                      <input type="text" name="contact number" id="contact number" autocomplete="family-name" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-600 focus:border-blue-600 border-warm-gray-300 rounded-md">
+                      <input type="text" 
+                      name="contact number" 
+                      id="contact number"
+                      value="{{ old('contact number') }}"
+                      autocomplete="family-name" 
+                      class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-600 focus:border-blue-600 border-warm-gray-300 rounded-md"
+                      >
+
+                      @error('title')
+                          <p class="text-red-500 text-lg">{{ $message }}</p>
+                      @enderror
+
                     </div>
                   </div>
 
                   <div>
                     <label for="email" class="block text-lg font-medium text-warm-gray-900">Email</label>
                     <div class="mt-1">
-                      <input id="email" name="email" type="email" autocomplete="email" class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-600 focus:border-blue-600 border-warm-gray-300 rounded-md">
+                      <input id="email" 
+                      name="email" 
+                      type="email" 
+                      value="{{ old('email') }}"
+                      autocomplete="email" 
+                      class="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-600 focus:border-blue-600 border-warm-gray-300 rounded-md"
+                      >
+                      @error('title')
+                          <p class="text-red-500 text-lg">{{ $message }}</p>
+                      @enderror
                   </div>
                 </div>
 
