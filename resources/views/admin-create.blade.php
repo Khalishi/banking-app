07d72cf769f4be99c32d5073228f27568d506c5d
= <=>
2023-02-28 12:50:54 +0200

    update the footer

--- resources/views/admin-create.blade.php
+++ resources/views/admin-create.blade.php
@@ -1,6 +1,4 @@
 <x-layout>
-        <!-- Contact form -->
-
         <div class="relative py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12 ml-96">
              <h3 class="text-lg font-medium text-blue-600">Create Contact-Information</h3>
                   <form action="/admin/contact-info/create" method="POST" class="mt-14">
